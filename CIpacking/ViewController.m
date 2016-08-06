b05eb9e0f2642889dac893ff076c5c724abe41ef
wyy <wangyayu@wxchina.com>
2016-08-06 16:27:29 +0800

    add all

---
+++ CIpacking/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CIpacking
+//
+//  Created by wyy on 16/8/6.
+//  Copyright © 2016年 yyx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
