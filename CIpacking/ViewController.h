c0f47363b3bcc0e44cfa83e04b0a98ade37fc08e
wyy <wangyayu@wxchina.com>
2016-08-06 16:08:47 +0800

    remove

---
+++ CIpacking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CIpacking
+//
+//  Created by wyy on 16/8/6.
+//  Copyright © 2016年 yyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
